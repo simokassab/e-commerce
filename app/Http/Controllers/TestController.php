2845edae70716fb8788c93f2af193879c825c332
Mohammad Azzam <mohammad.azzam.developer@gmail.com>
2022-05-13 10:54:27 +0300

    merged with unit and settings

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,20 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
+use App\Models\User\User;
 use Spatie\Permission\Traits\HasRoles;
 use Spatie\Permission\Models\Role;
-use App\Models\RolePermission;
 use Illuminate\Support\Arr;
 use Spatie\Permission\Models\Permission;
-use App\Services\RolesAndPermissionsService;
 
 class TestController extends Controller
 {
     use HasRoles;
 
-    public function test(){
-
-        $permissionsToPermit = Permission::findMany([1,2,3]);
+    public function getToken(){
+        return User::first();
     }
 }
