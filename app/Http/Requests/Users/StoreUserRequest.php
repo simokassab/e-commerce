54480f5d475592206ad64db62180971d386573b2
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-05-25 09:43:29 +0300

    ..

--- app/Http/Requests/Users/StoreUserRequest.php
+++ app/Http/Requests/Users/StoreUserRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Users;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreUser extends FormRequest
+class StoreUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
