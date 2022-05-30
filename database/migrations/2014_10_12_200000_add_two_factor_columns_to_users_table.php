62a13046b17d78544021f77f03631ca4525706f4
omat848 <66802550+omat848@users.noreply.github.com>
2022-05-30 15:37:54 +0300

    fixed eror fortify

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Laravel\Fortify\Fortify;
 
 return new class extends Migration
 {
@@ -23,11 +22,6 @@ return new class extends Migration
                     ->after('two_factor_secret')
                     ->nullable();
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')
-                        ->after('two_factor_recovery_codes')
-                        ->nullable();
-            }
         });
     }
 
