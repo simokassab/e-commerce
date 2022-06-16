830b44ab401769ac331a015016dc7924e9f92203
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-06-16 14:08:18 +0300

    fixed every thing

--- routes/api.php
+++ routes/api.php
@@ -40,67 +40,67 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
     Route::get('test',[TestController::class,'test']);
 
-Route::post('login', [AuthenticationController::class,'login'])->name('login');
-Route::get('logout', [AuthenticationController::class,'logout'])->name('logout');
+    Route::post('login', [AuthenticationController::class,'login'])->name('login');
+    Route::get('logout', [AuthenticationController::class,'logout'])->name('logout');
 
-Route::group([ 'prefix' => 'dashboard','middleware' => ['auth:sanctum','localization'] ],function (){
+    Route::group([ 'prefix' => 'dashboard','middleware' => ['auth:sanctum','localization'] ],function (){
 
-    //Permission
-    Route::get('get-nested-permissions/{permission}',[PermissionController::class,'getNestedPermissions']);
+        //Permission
+        Route::get('get-nested-permissions/{permission}',[PermissionController::class,'getNestedPermissions']);
 
-    // Routes Macro
-    Route::customBrandResource('brand', BrandController::class);
-    Route::customCategoryResource('category', CategoryController::class);
-    Route::customLanguageResource('language',LanguageController::class);
-    //End of Routes Macro
+        // Routes Macro
+        Route::customBrandResource('brand', BrandController::class);
+        Route::customCategoryResource('category', CategoryController::class);
+        Route::customLanguageResource('language',LanguageController::class);
+        //End of Routes Macro
 
-    Route::apiResource('attribute',AttributeController::class);
-    Route::post('attribute/all',[AttributeController::class,'index']); // for search
+        Route::apiResource('attribute',AttributeController::class);
+        Route::post('attribute/all',[AttributeController::class,'index']); // for search
 
-    Route::apiResource('attribute-value',AttributeValueController::class);
-    Route::post('attribute-value/all',[AttributeValueController::class,'index']); // for search
+        Route::apiResource('attribute-value',AttributeValueController::class);
+        Route::post('attribute-value/all',[AttributeValueController::class,'index']); // for search
 
-    Route::apiResource('country',CountryController::class);
-    Route::post('country/all',[CountryController::class,'index']); // for search
+        Route::apiResource('country',CountryController::class);
+        Route::post('country/all',[CountryController::class,'index']); // for search
 
-    Route::patch('currency/set-is-default/{id}',[CurrencyController::class,'setCurrencyIsDefault']);
-    Route::post('currency/all',[CurrencyController::class,'index']); // for search
-    Route::apiResource('currency',CurrencyController::class);
+        Route::patch('currency/set-is-default/{id}',[CurrencyController::class,'setCurrencyIsDefault']);
+        Route::post('currency/all',[CurrencyController::class,'index']); // for search
+        Route::apiResource('currency',CurrencyController::class);
 
-    Route::apiResource('discount',DiscountController::class);
-    Route::post('discount/all',[DiscountController::class,'index']); // for search
+        Route::apiResource('discount',DiscountController::class);
+        Route::post('discount/all',[DiscountController::class,'index']); // for search
 
-    Route::apiResource('discount-entity',DiscountEntityController::class);
-    Route::post('discount-entity/all',[DiscountEntityController::class,'index']); // for search
+        Route::apiResource('discount-entity',DiscountEntityController::class);
+        Route::post('discount-entity/all',[DiscountEntityController::class,'index']); // for search
 
-    Route::apiResource('field',FieldsController::class);
-    Route::post('field/all',[FieldsController::class,'index']);// for search
+        Route::apiResource('field',FieldsController::class);
+        Route::post('field/all',[FieldsController::class,'index']);// for search
 
-    Route::apiResource('field-value',FieldValueController::class);
-    Route::post('field-value/all',[FieldValueController::class,'index']);// for search
+        Route::apiResource('field-value',FieldValueController::class);
+        Route::post('field-value/all',[FieldValueController::class,'index']);// for search
 
-    Route::apiResource('label',LabelController::class);
-    Route::post('label/all',[LabelController::class,'index']);// for search
+        Route::apiResource('label',LabelController::class);
+        Route::post('label/all',[LabelController::class,'index']);// for search
 
-    Route::apiResource('role',RolesController::class);
-    Route::post('role/all',[RolesController::class,'index']);// for search
+        Route::apiResource('role',RolesController::class);
+        Route::post('role/all',[RolesController::class,'index']);// for search
 
-    Route::apiResource('setting',SettingsController::class);
-    Route::post('setting/all',[SettingsController::class,'index']);// for search
+        Route::apiResource('setting',SettingsController::class);
+        Route::post('setting/all',[SettingsController::class,'index']);// for search
 
-    Route::apiResource('tag',TagController::class);
-    Route::post('tag/all',[TagController::class,'index']);// for search
+        Route::apiResource('tag',TagController::class);
+        Route::post('tag/all',[TagController::class,'index']);// for search
 
-    Route::apiResource('unit',UnitController::class);
-    Route::post('unit/all',[UnitController::class,'index']);// for search
+        Route::apiResource('unit',UnitController::class);
+        Route::post('unit/all',[UnitController::class,'index']);// for search
 
 
-    Route::get('/profile', function(Request $request) {
-        return auth()->user();
+        Route::get('/profile', function(Request $request) {
+            return auth()->user();
+        });
+
+        Route::apiResource('user',UserController::class);
+        Route::post('user/all',[UserController::class,'index']);
     });
-
-    Route::apiResource('user',UserController::class);
-    Route::post('user/all',[UserController::class,'index']);
-});
 
     Route::get('s',[AttributeController::class,'serachdata']);
