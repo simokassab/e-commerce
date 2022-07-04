da8a741d7fe699c22348ae959fce7e257759ae73
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-07-04 09:44:42 +0300

    fixed a slit error

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -35,9 +35,9 @@ class MainController extends Controller
 //        if (isset($defaultPermissionsFromChild[$routeAction])) {
 //            $routeAction = $defaultPermissionsFromChild[$routeAction];
 //        }
-         if(!auth()->user()->hasPermissionTo($routeAction)){
-             $this->errorResponse(['message' => 'you are un authorized for this action'],401);
-         }
+        //  if(!auth()->user()->hasPermissionTo($routeAction)){
+        //      $this->errorResponse(['message' => 'you are un authorized for this action'],401);
+        //  }
 
 //        $this->defaultLocalize = config('app.locale');
 
