c9fbac5ed85b520d37ba5e96cd59e02737c22af5
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-07-08 11:06:28 +0300

    Fix Price Resource

--- app/Http/Resources/Price/PriceResource.php
+++ app/Http/Resources/Price/PriceResource.php
@@ -13,17 +13,27 @@ class PriceResource extends JsonResource
      */
     public function toArray($request)
     {
-        $arrayForVirtualInfo = [];
+        // $arrayForVirtualInfo = [];
 
 
-        $arrayForVirtualInfo['id']  =$this->id;
-        $arrayForVirtualInfo['name']  =$this->name ?? '-';
-        $arrayForVirtualInfo['is_virtual']  = $this->is_virtual;
-        $arrayForVirtualInfo['currency']  =($this->whenLoaded('currency')->code .' - '.$this->whenLoaded('currency')->symbol)  ?? '-';
-        $arrayForVirtualInfo['original_price_id'] = ($this->whenLoaded('originalPrice')->name) ?? '-';
-        $arrayForVirtualInfo['original_percent'] = ($this->percentage) ?? '-';
+        // $arrayForVirtualInfo['id']  =$this->id;
+        // $arrayForVirtualInfo['name']  =$this->name ?? '-';
+        // $arrayForVirtualInfo['is_virtual']  = $this->is_virtual;
+        // $arrayForVirtualInfo['currency']  =($this->whenLoaded('currency')->code .' - '.$this->whenLoaded('currency')->symbol)  ?? '-';
+        // $arrayForVirtualInfo['original_price_id'] = ($this->whenLoaded('originalPrice')->name) ?? '-';
+        // $arrayForVirtualInfo['original_percent'] = ($this->percentage) ?? '-';
 
-        return $arrayForVirtualInfo;
+        // return $arrayForVirtualInfo;
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_virtual' => $this->is_virtual,
+            'currency' => ($this->whenLoaded('currency')->code .' - '.$this->whenLoaded('currency')->symbol)  ?? '-',
+            'original_price_id' => ($this->whenLoaded('originalPrice')->name) ?? '-',
+            'original_percent' => (round($this->original_percent,config('defaults.default_round_percentage'))) ?? '-',
+            'percentage' => (round($this->percentage,config('defaults.default_round_percentage'))) ?? '-',
+        ];
 
     }
 }
