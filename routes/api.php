f7a57c5b83e8b91f8bde70803b901f5ca5b13b2a
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-07-14 15:38:10 +0300

    working... testing..

--- routes/api.php
+++ routes/api.php
@@ -102,8 +102,8 @@ Route::group([ 'prefix' => 'dashboard','middleware' => $dashboardMiddleware ],fu
     Route::apiResource('user',UsersController::class);
     Route::post('user/all',[UsersController::class,'index']);//for searching
 
-    Route::post('tax/all',[TaxController::class,'index']);//for searching
     Route::apiResource('tax',TaxController::class);
+    Route::post('tax/all',[TaxController::class,'index']);//for searching
 
     Route::post('price/all',[PricesController::class,'index']);// for search
     Route::get('price/get-original-prices',[PricesController::class,'getOriginalPrices'])->name('get.original.prices');
