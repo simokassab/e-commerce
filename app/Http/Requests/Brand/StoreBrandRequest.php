087665a9a2c7cc3239b7367fe108fb986af04373
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-07-20 16:24:37 +0300

    testing

--- app/Http/Requests/Brand/StoreBrandRequest.php
+++ app/Http/Requests/Brand/StoreBrandRequest.php
@@ -44,9 +44,9 @@ class StoreBrandRequest extends MainRequest
             'description' => 'nullable',
             'sort' => 'nullable | integer',
 
-            'fields.*.field_id' => 'required | exists:fields,id,entity,brand',
+//            'fields.*.field_id' => 'required | exists:fields,id,entity,brand',
 //            'fields.*.field_value_id' =>  'integer | exists:fields_values,id',
-            'fields.*.value'=> 'nullable',
+//            'fields.*.value'=> 'nullable',
 
             'labels.*' => 'required | exists:labels,id',
 
