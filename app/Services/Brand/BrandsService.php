5c3bce136e16105e048d63be64fdb662ea74bca3
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-07-20 17:27:43 +0300

    tetsing

--- app/Services/Brand/BrandsService.php
+++ app/Services/Brand/BrandsService.php
@@ -29,7 +29,6 @@ class BrandsService {
         $fieldsArray = $fields;
         foreach ($fields as $key => $field){
             $field = (array)json_decode($field);
-            return gettype($field);
             if($fieldsArray[$key]["type"]=='select' && gettype($field['value']) == 'integer' ){
                 $fieldsArray[$key]["value"] = null;
                 $fieldsArray[$key]["field_value_id"] = $field["value"];
