5ddd1f4271eb9e614d94308d3acebbc900544a20
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-07-21 11:28:47 +0300

    testing

--- app/Services/Brand/BrandsService.php
+++ app/Services/Brand/BrandsService.php
@@ -27,7 +27,8 @@ class BrandsService {
 
     public static function addFieldsToBrands(Brand $brand, array $fields){
         $fieldsArray = $fields;
-        return $fieldsArray;
+        return $fieldsArray = (array)json_decode($fieldsArray);
+
         foreach ($fields as $key => $field){
             $field = (array)json_decode($field);
             if($fieldsArray[$key]["type"]=='select' && gettype($field['value']) == 'integer' ){
