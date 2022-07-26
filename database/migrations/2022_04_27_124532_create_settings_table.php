39a216be7731ec620a798e03982dc34cc31537b3
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-07-26 11:20:19 +0300

    working

--- database/migrations/2022_04_27_124532_create_settings_table.php
+++ database/migrations/2022_04_27_124532_create_settings_table.php
@@ -16,7 +16,8 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('title',250)->unique();
-            $table->string('value',250);
+            $table->text('value')->nullable();
+            $table->enum('type', ['number', 'text','checkbox','select','multi-select']);
             $table->boolean('is_developer')->default(0);
             $table->timestamps();
         });
