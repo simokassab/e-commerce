9d433ba1e82fd9aede3a5dbb54afdd1c22839b7d
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-03 01:08:23 +0300

    working..

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -135,7 +135,6 @@ class ProductService
                     'updated_at' => Carbon::now()->toDateString(),
                 ];
             }
-            return $data;
             if (ProductImage::insert($data)) {
                 return $this;
             }
