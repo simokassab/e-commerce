8ae2d7cfe2799949475e5a45a75576c6d9090478
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-03 01:36:38 +0300

    working

--- app/Models/Product/ProductImage.php
+++ app/Models/Product/ProductImage.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Product;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\MainModel;
 use App\Models\Product\Product;
@@ -12,6 +13,14 @@ class ProductImage extends MainModel
     use HasFactory,HasTranslations;
     protected $table='products_images';
     protected array $translatable = ['title'];
+    protected $fillable = [
+        'product_id',
+        'image',
+        'title',
+        'sort',
+        'created_at' ,
+        'updated_at',
+    ];
 
     public function productImages(){
         return $this->belongsTo(Product::class,'product_id');
