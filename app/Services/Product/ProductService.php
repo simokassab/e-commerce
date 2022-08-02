906aeb25083971fb25aee862c65e1d1b97ef1b3d
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-02 18:13:28 +0300

    working and testing

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -119,9 +119,7 @@ class ProductService
              throw new Exception('Images and images_data count is not equal');
          }
 
-        $childrenIdsArray = array();
-
-        $childrenIdsArray[] = $childrenIds;
+        $childrenIdsArray = $childrenIds;
         $childrenIdsArray[] = $productId;
 
         $data = [];
