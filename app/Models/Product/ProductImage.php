cf0edd7086edc6f7d32a91d2d31ffa9cde8b6629
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-03 01:23:39 +0300

    working..

--- app/Models/Product/ProductImage.php
+++ app/Models/Product/ProductImage.php
@@ -5,12 +5,14 @@ namespace App\Models\Product;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\MainModel;
 use App\Models\Product\Product;
+use Spatie\Translatable\HasTranslations;
 
 class ProductImage extends MainModel
 {
-    use HasFactory;
+    use HasFactory,HasTranslations;
     protected $table='products_images';
     protected $guard_name = 'web';
+    protected array $translatable = ['title'];
 
     public function productImages(){
         return $this->belongsTo(Product::class,'product_id');
