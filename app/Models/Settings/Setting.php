5423060bd38f20c162f10e1abee40a91b5cf12f2
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-05 10:22:17 +0300

    fix settings

--- app/Models/Settings/Setting.php
+++ app/Models/Settings/Setting.php
@@ -4,6 +4,7 @@ namespace App\Models\Settings;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\MainModel;
+use App\Models\Price\Price;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Support\Facades\Cache;
 
@@ -11,26 +12,72 @@ class Setting extends MainModel
 {
     use HasFactory;
 
-    protected $translatable=[];
-    protected $fillable = ['title','value','type','is_developer'];
+    protected $translatable = [];
+    protected $fillable = ['title', 'value', 'type', 'is_developer'];
+
+    // public function getPriceOptions(){
+    //     dd($prices);
+    // }
 
     public static $titlesOptions = [
-        'products_required_fields' =>  ['sku','summary','specification','barcode','length','height','width','weight','brand_id','tax_id'],
-        'products_quantity_greater_than_or_equal'=>[],
-        'products_minimum_and_reserved_quantity_greater_than_or_equal'=>[],
-        'products_prices_greater_than_or_equal'=>[],
-        'products_discounted_price_greater_than_or_equal'=>[],
-        'website_pricing'=> [],
+        'products_required_fields' =>  [
+            [
+                'id' => 'sku',
+                'name' => 'sku'
+            ],
+            [
+                'id' => 'summary',
+                'name' => 'summary'
+            ],
+            [
+                'id' => 'specification',
+                'name' => 'specification'
+            ],
+            [
+                'id' => 'barcode',
+                'name' => 'barcode'
+            ],
+            [
+                'id' => 'length',
+                'name' => 'length'
+            ],
+            [
+                'id' => 'height',
+                'name' => 'height'
+            ],
+            [
+                'id' => 'width',
+                'name' => 'width'
+            ],
+            [
+                'id' => 'weight',
+                'name' => 'weight'
+            ],
+            [
+                'id' => 'brand_id',
+                'name' => 'brand_id'
+            ],
+            [
+                'id' => 'tax_id',
+                'name' => 'tax_id'
+            ],
+
+        ],
+        'products_quantity_greater_than_or_equal' => [],
+        'products_minimum_and_reserved_quantity_greater_than_or_equal' => [],
+        'products_prices_greater_than_or_equal' => [],
+        'products_discounted_price_greater_than_or_equal' => [],
+        'website_pricing' => [],
     ];
 
-    public static function validateOptionsByTitle($keyTitle,$givenOptions){
+    public static function validateOptionsByTitle($keyTitle, $givenOptions)
+    {
+
+
         $returnBool = true; //1. if the title is not in the array of titlesOptions, return true
         foreach ($givenOptions as $option)
             $returnBool &= in_array($option, Setting::$titlesOptions[$keyTitle]);
 
         return $returnBool;
     }
-
-
-
 }
