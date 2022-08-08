72e3819ed38f825d6c4b9ff8fae8e4efdc4e42f5
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-08 17:12:25 +0300

    variable

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -355,7 +355,7 @@ class ProductService
         $product->minimum_quantity = $request->minimum_quantity;
         $product->summary = ($request->summary);
         $product->specification = ($request->specification);
-        if ($request->image && !empty($request->image))
+        if (!is_null($request->image) && !empty($request->image))
             $product->image = uploadImage($request->image, config('images_paths.product.images'));
 
         $product->meta_title = $request->meta_title ?? "";
