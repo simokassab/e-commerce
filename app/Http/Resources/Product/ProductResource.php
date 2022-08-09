4ecf8b74fbe83038f954538a4f9edf89e718cd3f
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-09 10:41:17 +0300

    variable

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,7 +18,6 @@ class ProductResource extends JsonResource
     {
 
 
-        dd($this->whenLoaded('defaultCategory') ? $this->whenLoaded('defaultCategory')->name : "-");
         $data[0]['name'] = $this->whenLoaded('defaultCategory') ? $this->whenLoaded('defaultCategory')->name : "-";
         $data[0]['isMain'] = true;
         $categories = [];
