bfa5912e1bfed6058f206fd9f5e30f155baff6fe
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-10 15:13:53 +0300

    product

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -406,7 +406,7 @@ class ProductService
         // $finalPricesCollect = collect($data)->collapse()->toArray();
         ProductPrice::insert($data);
 
-            // $this->storeImagesForVariations($request,$childrenIds);
+            $this->storeImagesForVariations($request,$childrenIds);
             // $this->storeFieldsForVariations($request,$childrenIds);
 
         if (count($childrenIds) > 0) {
