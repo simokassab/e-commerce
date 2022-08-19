860f9e470db3348502a1fac3821123f2e1ef3dc2
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-19 22:16:44 +0300

    working on shipping as billinf boolean

---
+++ database/migrations/2022_08_19_190707_add_cloumn_shipping_as_billing_on_table_orders.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders',function (Blueprint $table){
+            if(Schema::hasColumn('orders','is_billing_as_shipping')){
+                $table->boolean('is_billing_as_shipping')->after('updated_at')->default(0);
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders',function (Blueprint $table){
+            if(Schema::hasColumn('orders','is_billing_as_shipping')){
+                $table->dropColumn('is_billing_as_shipping');
+            }
+        });
+    }
+};
