bf73ad43d7ba37722b784a7b71c8503b856a3260
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-23 14:43:00 +0300

    ..

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -581,9 +581,9 @@ class ProductService
                $childrenData=Product::where('parent_product_id',$product->id)->get();
                dd($childrenData[0]->id);
             foreach ($childrenData as $key => $child) {
-                $childrenIds[$key]=$child[$key]['id'];
+                $childrenIds[$key]=$child[$key]->id;
             }
-            //    dd($childrenIds);
+               dd($childrenIds);
 
                 $this->storeImagesForVariations($request, $childrenIds);
                 $this->storePricesForVariations($request, $childrenIds);
