de32dcccc860c1fd71ff591ea62d15b28e536f83
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-23 16:33:35 +0300

    .

--- app/Http/Resources/Product/SingleProductResource.php
+++ app/Http/Resources/Product/SingleProductResource.php
@@ -69,7 +69,7 @@ class SingleProductResource extends JsonResource
             'is_show_related_product' => $this->is_show_related_product,
             'website_status' => $this->website_status,
             'pre_order' => $this->pre_order ?? 0,
-            'prices' => SelectPriceResource::collection($this->whenLoaded('price')->load('currency')) ?? [],
+            'prices' => SelectPriceResource::collection($this->whenLoaded('priceClass')->load('currency')) ?? [],
             'fields' => SingleFieldResource::collection($this->whenLoaded('field'))->where('is_attribute',0) ?? [],
             'attributes' => SingleFieldResource::collection($this->whenLoaded('field'))->where('is_attribute',1) ?? [],
             'tags' => TagResource::collection($this->whenLoaded('tags')),
