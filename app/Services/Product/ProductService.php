6efbb66aed53932b5d9ed6c080492786551c7393
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-23 14:28:14 +0300

    ..

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -576,7 +576,7 @@ class ProductService
             }
             $model = new Product();
             $productVariation = Product::upsert($productVariationParentsArray,'id',$model->getFillable());
-
+            dd($productVariation);
             foreach ($productVariation as $key => $variation) {
                 $childrenIds[] = $productVariation->id;
             }
