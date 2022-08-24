f34b4245873d751532973ae649c20748ac7832d4
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-24 14:04:31 +0300

    product price

--- app/Http/Resources/Product/ProductPriceResoruce.php
+++ app/Http/Resources/Product/ProductPriceResoruce.php
@@ -15,7 +15,7 @@ class ProductPriceResoruce extends JsonResource
     public function toArray($request)
     {
         $price = $this->whenLoaded('prices');
-        dd($price['currency']);
+        dd($price->whenLoaded('currency'));
         return [
             'id' => $this->id,
             'product_id' => (int)$this->product_id,
