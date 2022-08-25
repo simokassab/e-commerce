4a69af3cc43f92cc202e2e1f0828b6e3d773ce60
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-25 23:14:19 +0300

    finshed hxa

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -10,6 +10,7 @@ use Spatie\Permission\Models\Role;
 
 class AuthenticationController extends MainController
 {
+
     public function login(LoginRequest $request){
 
         $validated = $request->validate([
@@ -39,7 +40,7 @@ class AuthenticationController extends MainController
 
     }
 
-    public function ThirdPartylogin(LoginRequest $request){
+    public function thirdPartyLogin(LoginRequest $request){
 
         $validated = $request->validate([
             'email' => 'required|email',
@@ -50,19 +51,17 @@ class AuthenticationController extends MainController
             return $this->errorResponse('Sorry, but you entered the wrong credentials!',[],-1,401);
         }
 
-        $permissions = [];
 
-        if(!is_null(\auth()->user()->roles) && count(\auth()->user()->roles) > 0){
-            $permissions = \auth()->user()->roles[0]->permissions;
+        if(is_null(\auth()->user()->roles) || \auth()->user()->roles[0]->name != 'hxa'){
+            Auth::logout();
+            return $this->errorResponse(message:'Error! the user you logged in is not for general APIs', statusCode:403);
         }
+
 
         return $this->successResponse(
             'Authenticated Successfully! ',
             [
-                'user' => \auth()->user(),
-                'permissions' => $permissions,
                 'token' => \auth()->user()->createToken('app-token', ['service'])->plainTextToken
-
             ]
             ,1,202);
 
@@ -73,4 +72,13 @@ class AuthenticationController extends MainController
         Auth::logout();
         return $this->successResponse('Logout Successfully!');
     }
+
+    public static function thirdPartyLogout(){
+        if(\auth()->user()){
+        \auth()->user()->tokens()->delete();
+        }
+        Auth::logout();
+
+    }
+
 }
