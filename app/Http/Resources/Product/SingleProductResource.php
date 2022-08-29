20682af59baf3db44a894cc7586a3a4c1cef4791
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-29 23:57:24 +0300

    related products

--- app/Http/Resources/Product/SingleProductResource.php
+++ app/Http/Resources/Product/SingleProductResource.php
@@ -42,7 +42,7 @@ class SingleProductResource extends JsonResource
         $productAttributes = ProductField::whereIn('product_id',$childrenIds)->get();
 
     $productRelatedIds=collect($this->whenLoaded('productRelatedChildren'))->pluck('child_product_id');
-    $productsRelatedNames=Product::findMany($productRelatedIds->toArray())->name;
+    $productsRelatedNames=Product::findMany($productRelatedIds->toArray())->pluck('name');
     dd($productsRelatedNames);
         return [
             'id' => (int)$this->id,
