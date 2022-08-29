73ffa8cf5ce7666d384f46e15a8471485b27d828
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-08-30 01:01:26 +0300

    working

--- app/Http/Controllers/Prices/PricesListController.php
+++ app/Http/Controllers/Prices/PricesListController.php
@@ -46,7 +46,6 @@ class PricesListController extends MainController
             'general_search' => 'nullable',
             'limit' => 'nullable',
             'data.*' => 'nullable',
-            'data' => 'required',
             'advanced_search.prices_class' => 'required',
             'advanced_search.prices_class.*' => 'required|numeric|exists:prices,id',
 
