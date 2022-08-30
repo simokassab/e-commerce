0dd0cdab411618672bca0a90e3da2e8824e717f6
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-08-30 10:19:35 +0300

    update product

--- app/Http/Resources/Product/SingleProductResource.php
+++ app/Http/Resources/Product/SingleProductResource.php
@@ -44,7 +44,7 @@ class SingleProductResource extends JsonResource
         $productRelatedIds = collect($this->whenLoaded('productRelatedChildren'))->pluck('child_product_id');
         $model=new Product();
         $productsRelatedNames = Product::find($productRelatedIds->toArray())->toArray();
-        $p=array_pluck($productsRelatedNames,$model->getTranslations('name'));
+dd($productsRelatedNames);
         $productRelated = ($this->whenLoaded('productRelatedChildren'))->toArray();
 
         foreach ($productRelated as $key => $product) {
