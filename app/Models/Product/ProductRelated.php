e519f759f70f4fbdb3139320e60d3bb83ff8e980
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-09-01 15:14:37 +0300

    working

--- app/Models/Product/ProductRelated.php
+++ app/Models/Product/ProductRelated.php
@@ -4,11 +4,12 @@ namespace App\Models\Product;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class ProductRelated extends Model
 {
-    use HasFactory;
+    use HasFactory,HasTranslations;
     protected $table = 'products_related';
-
+    protected $translatable= ['name'];
     protected $guarded = [];
 }
