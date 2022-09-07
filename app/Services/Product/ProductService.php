9433ec4a8d6bf4e015fa01484b230a52e6bd5451
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-09-08 01:06:59 +0300

    working

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -146,7 +146,7 @@ class ProductService
 
             $data = [];
             $allData = [];
-            dd(collect($request->toArray()['product_variations'])->pluck('attributes')->unique());
+            dd(collect($request->toArray()['product_variations'])->pluck('attributes')->unique(fn($item) => $item['field_id'].$item['value'] ));
 
             foreach ($request->attributes as $index => $attribute) {
 
