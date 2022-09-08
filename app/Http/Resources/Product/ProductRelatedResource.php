d1e11e06081037e843546c89f7319bac16593d80
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-09-08 09:46:30 +0300

    ,

--- app/Http/Resources/Product/ProductRelatedResource.php
+++ app/Http/Resources/Product/ProductRelatedResource.php
@@ -32,7 +32,6 @@ class ProductRelatedResource extends JsonResource
             'id' => (int)$this->child_product_id,
             'child_quantity' => (int)$this->child_quantity,
             'name' => $this->getTranslations('name'),
-            'child_name_status' => $this->child_name_status,
             'name_original' => $product->getTranslations('name'),
             'name_status' => $this->child_name_status,
             'images' => ProductImagesResource::collection($images) ?? [],
