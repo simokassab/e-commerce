1f5544c0685c5d82965f3c543cdf81be431efcfd
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-09-08 03:32:35 +0300

    working

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -138,7 +138,7 @@ class ProductService
     {
 //        DB::beginTransaction();
         try {
-            if(!array_key_exists('attributes',$request->toArray)){
+            if(!array_key_exists('attributes',$request->toArray())){
                 return $this;
             }
 
