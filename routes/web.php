feed16a8c05842eb73e891614176e30d18d029ac
Ahmad Hijazi <40335765+ahmad96hijazi@users.noreply.github.com>
2022-09-17 16:51:12 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('{any}', function () {
     return view('index');
-});
-
+})->where('any','.*');
 
 if (config('app.debug')) {
     // for development purposes only -----------------------------------------------------------------
