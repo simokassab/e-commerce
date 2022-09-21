2f98aef7d81622f546d4fe4850d5847a5ee732c4
mohammad azzam <mohammad.azzam.developer@gmail.com>
2022-09-21 18:24:58 +0300

    working..

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -109,7 +109,6 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->renderable(function (Throwable $exception, $request) {
-            return $exception->getMessage();
             if ($request->wantsJson()) {
                 return parent::prepareJsonResponse($request, $exception);
             }
