98a4a5012aa10cd520fcf2c048dc585dd5db830b
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-09-21 10:39:27 +0300

    fix labels in single brand resource

--- app/Http/Resources/Brand/SingleBrandResource.php
+++ app/Http/Resources/Brand/SingleBrandResource.php
@@ -20,7 +20,7 @@ class SingleBrandResource extends JsonResource
     public function toArray($request)
     {
 
-        $labels = $this->whenLoaded('label') ? $this->whenLoaded('label') : $this->whenLoaded('label')->pluck('id');
+        $labels = $this->whenLoaded('label') ?  $this->whenLoaded('label')->pluck('id') :[];
         $fieldsValues = $this->whenLoaded('fieldValue');
 
 
