d44d9eeedf840a25a9e1998c9402593395d15073
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-09-23 15:52:31 +0300

    .

--- app/Http/Controllers/Country/CountryController.php
+++ app/Http/Controllers/Country/CountryController.php
@@ -112,7 +112,7 @@ class CountryController extends MainController
      * @param int $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function update(StoreCountryRequest $request, Country $country)
+    public function update(UpdateCountryRequest $request, Country $country)
     {
         $dataTranslatable = (array)json_decode($request->name);
         $country->name = ($dataTranslatable);
