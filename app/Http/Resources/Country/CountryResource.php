dd8f3489b670de4633aa43b5e537dd38abd3fa33
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-09-23 09:36:05 +0300

    .

--- app/Http/Resources/Country/CountryResource.php
+++ app/Http/Resources/Country/CountryResource.php
@@ -16,11 +16,11 @@ class CountryResource extends JsonResource
     {
          return [
             'id' => $this->id,
+            'flag'=> $this->flag && !empty($this->flag) ?  getAssetsLink('storage/'.$this->flag): 'default_image' ,
             'name'=>$this->name,
             'iso_code_1'=>$this->iso_code_1,
             'iso_code_2'=>$this->iso_code_2,
             'phone_code'=>$this->phone_code,
-            'flag'=> $this->flag && !empty($this->flag) ?  getAssetsLink('storage/'.$this->flag): 'default_image' ,
 
         ];
     }
