1f8d80f99736bc780ce61924118b0fbdabdd0e63
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-09-27 14:47:26 +0300

    create trigger

--- app/Observers/Currency/CurrencyObserver.php
+++ app/Observers/Currency/CurrencyObserver.php
@@ -2,7 +2,32 @@
 
 namespace App\Observers\Currency;
 
+use App\Models\Currency\Currency;
+use App\Models\Currency\CurrencyHistory;
+use Illuminate\Support\Carbon;
+
 class CurrencyObserver
 {
-    //
+    public function created(Currency $currency)
+    {
+        $data[] = [
+            'id' => $currency->id,
+            'rate' => $currency->rate,
+            'created_at'  => Carbon::now()->toDateString(),
+            'updated_at' => Carbon::now()->toDateString(),
+        ];
+        dd($data);
+        CurrencyHistory::create($data);
+    }
+
+    public function updated(Currency $currency)
+    {
+        $data[] = [
+            'id' => $currency->id,
+            'rate' => $currency->rate,
+            'created_at'  => Carbon::now()->toDateString(),
+            'updated_at' => Carbon::now()->toDateString(),
+        ];
+        CurrencyHistory::create($data);
+    }
 }
