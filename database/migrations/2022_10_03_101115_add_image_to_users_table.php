d46d1d3f14b91d1a39938bb2e8781d4b426d1576
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-10-03 13:40:30 +0300

    finish user image

--- database/migrations/2022_10_03_101115_add_image_to_users_table.php
+++ database/migrations/2022_10_03_101115_add_image_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('image')->nullable();
+            $table->text('image')->after('remember_token')->nullable();
         });
     }
 
