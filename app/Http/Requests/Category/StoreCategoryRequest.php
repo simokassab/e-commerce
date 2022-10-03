06dee8e6cb9e7ecdd5d9c914dbe3941ce37e38ca
Bilal Abo Jamous <101055860+Bilal-AJ0@users.noreply.github.com>
2022-10-03 17:38:40 +0300

    C:/Program Files/Git/

--- app/Http/Requests/Category/StoreCategoryRequest.php
+++ app/Http/Requests/Category/StoreCategoryRequest.php
@@ -69,6 +69,7 @@ class StoreCategoryRequest extends MainRequest
             if (!is_array($this->fields))
                 throw new Exception('fields must be of type array');
                 foreach ($this->fields as $key => $field) {
+                dd($field['value']);
                     if ($field['type'] == 'date') {
                     $fieldsRules[] = [
                         'fields.*.value' => 'required | date'
